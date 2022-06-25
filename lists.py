fe910f0d2a625bd038fff3d0086653d5d6a8f7f1
Tito Reinhart <TKral@pdx.edu>
2022-06-25 10:41:32 -0700

    having fun with list

--- lists.py
+++ lists.py
@@ -12,4 +12,18 @@ mixedLists = [5, True, myFaveFoods]
 for i in mixedLists :
     if myFaveFoods in mixedLists :
         print("yes")
-        break #don't like putting breaks in loops
+        break #don't like putting breaks in loops\
+
+if (len(myFaveFoods) >= len(mixedLists)) :
+    print("myFaveFoods is at least as long as mixed list:", 
+    len(myFaveFoods), " number of items")
+    myFaveFoods.append("Burritos")
+    mixedLists.insert(len(mixedLists),False)
+    
+else : 
+    print("myFaveFoods is not as long as mixed list:", 
+    len(mixedLists), " number of items")
+    mixedLists.append(False)
+
+print(myFaveFoods)
+print(mixedLists)
