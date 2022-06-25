d2c121ca13c7d3647dc23499122f1626268440f4
Tito Reinhart <TKral@pdx.edu>
2022-06-25 10:35:21 -0700

    having fun programming

--- lists.py
+++ lists.py
@@ -7,4 +7,9 @@ for i in myFaveFoods :
 """
 
 mixedLists = [5, True, myFaveFoods]
-print(mixedLists[2][2])
+#print(mixedLists[2][2])
+
+for i in mixedLists :
+    if myFaveFoods in mixedLists :
+        print("yes")
+        break #don't like putting breaks in loops
