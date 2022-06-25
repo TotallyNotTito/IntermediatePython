a3646d3345df5b1066443822331153f508ba6739
Tito Reinhart <TKral@pdx.edu>
2022-06-25 10:30:29 -0700

    fun with mixed lists

--- lists.py
+++ lists.py
@@ -1,5 +1,10 @@
 myFaveFoods = ["Mac & Cheese", "Hot Dogs", "Mango", "Carrots"]
-print(myFaveFoods)
+#print(myFaveFoods)
 
+"""
 for i in myFaveFoods :
     print(i, end=", ")
+"""
+
+mixedLists = [5, True, myFaveFoods]
+print(mixedLists[2][2])
